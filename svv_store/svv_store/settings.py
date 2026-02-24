e044fc1c0cd051ae57c678844db707791632952a
vamshi <vamshi@smalldaytech.com>
2026-02-24 23:27:53 +0530

    db changes

--- svv_store/svv_store/settings.py
+++ svv_store/svv_store/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'svv_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
