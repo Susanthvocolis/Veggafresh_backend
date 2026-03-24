e9f0e611aa18931403080602e16e53b93fece2a1
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2026-03-24 15:17:19 +0530

    Update serializers.py

--- svv_store/cart/serializers.py
+++ svv_store/cart/serializers.py
@@ -1,3 +1,6 @@
+from rest_framework import serializers
+from .models import Cart, CartItem
+
 class CartItemSerializer(serializers.ModelSerializer):
     cart_item_id = serializers.IntegerField(source='id', read_only=True)
     product_id = serializers.IntegerField(source='product_variant.product.id', read_only=True)
@@ -35,3 +38,20 @@ class CartItemSerializer(serializers.ModelSerializer):
     def get_product_image(self, obj):
         image = obj.product_variant.product.images.first()
         return image.image if image else None
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = [
+            'id',
+            'user',
+            'items',
+            'total_amount',
+            'taxes',
+            'handling_charges',
+            'delivery_charges',
+            'final_amount',
+        ]
