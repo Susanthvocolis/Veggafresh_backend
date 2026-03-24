62c0863e8720dac8087ab40e75db0d91e4e75d30
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2026-03-24 14:10:54 +0530

    Update serializers.py

--- svv_store/cart/serializers.py
+++ svv_store/cart/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
 
-
 class CartItemSerializer(serializers.ModelSerializer):
-    product_variant = serializers.StringRelatedField()
+    cart_item_id = serializers.IntegerField(source='id', read_only=True)
+    product_id = serializers.IntegerField(source='product_variant.product.id', read_only=True)
     product_name = serializers.CharField(source='product_variant.product.name', read_only=True)
+    product_variant_id = serializers.IntegerField(source='product_variant.id', read_only=True)
+    product_variant = serializers.StringRelatedField()
     product_image = serializers.SerializerMethodField()
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product_variant', 'product_name', 'product_image', 'quantity']
+        fields = [
+            'cart_item_id',
+            'product_id',
+            'product_name',
+            'product_variant_id',
+            'product_variant',
+            'product_image',
+            'quantity',
+        ]
 
     def get_product_image(self, obj):
         image = obj.product_variant.product.images.first()
@@ -21,4 +31,13 @@ class CartSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items', 'total_amount', 'taxes', 'handling_charges', 'delivery_charges', 'final_amount']
+        fields = [
+            'id',
+            'user',
+            'items',
+            'total_amount',
+            'taxes',
+            'handling_charges',
+            'delivery_charges',
+            'final_amount',
+        ]
