cd6df4cb2fc1a157d7fab779c7d532bcb561f8a3
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2026-03-24 14:20:09 +0530

    Update serializers.py

--- svv_store/cart/serializers.py
+++ svv_store/cart/serializers.py
@@ -1,6 +1,3 @@
-from rest_framework import serializers
-from .models import Cart, CartItem
-
 class CartItemSerializer(serializers.ModelSerializer):
     cart_item_id = serializers.IntegerField(source='id', read_only=True)
     product_id = serializers.IntegerField(source='product_variant.product.id', read_only=True)
@@ -8,6 +5,18 @@ class CartItemSerializer(serializers.ModelSerializer):
     product_variant_id = serializers.IntegerField(source='product_variant.id', read_only=True)
     product_variant = serializers.StringRelatedField()
     product_image = serializers.SerializerMethodField()
+    price = serializers.DecimalField(
+        source='product_variant.price',
+        max_digits=10,
+        decimal_places=2,
+        read_only=True
+    )
+    discounted_price = serializers.DecimalField(
+        source='product_variant.discounted_price',
+        max_digits=10,
+        decimal_places=2,
+        read_only=True
+    )
 
     class Meta:
         model = CartItem
@@ -18,26 +27,11 @@ class CartItemSerializer(serializers.ModelSerializer):
             'product_variant_id',
             'product_variant',
             'product_image',
+            'price',
+            'discounted_price',
             'quantity',
         ]
 
     def get_product_image(self, obj):
         image = obj.product_variant.product.images.first()
         return image.image if image else None
-
-
-class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Cart
-        fields = [
-            'id',
-            'user',
-            'items',
-            'total_amount',
-            'taxes',
-            'handling_charges',
-            'delivery_charges',
-            'final_amount',
-        ]
